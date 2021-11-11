7d2d73988e4096c545f785a0f886e2df06562f98
mazen506 <mazen506@gmail.com>
2021-11-11 09:56:49 +0300

    fixes

--- config/app.php
+++ config/app.php
@@ -177,8 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TranslationServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
+        App\Providers\TranslationServiceProvider::class
 
     ],
 
@@ -233,7 +232,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
 
 
 
