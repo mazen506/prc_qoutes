cd0e3114d916b1a90b73238af53f33c50be4b8fc
mazen506 <mazen506@gmail.com>
2021-11-30 16:37:27 +0300

    fixing-display

--- resources/views/qoute_display.blade.php
+++ resources/views/qoute_display.blade.php
@@ -60,7 +60,7 @@
                             @foreach ($qoute->items as $item)
                                 <tr id="item{{ $loop->index }}">
                                     <td class='col-item-serial'>{{ $loop->index+1 }}</td>
-                                    <td class='col-item-image'>
+                                    <td class='col-item-image align-center'>
                                         @php 
                                             $item_images = $item->images;
                                             $item_image = explode("|", $item_images)[0];
