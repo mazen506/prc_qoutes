e7e61df0a47b130dea969170ec32e7517587ef5b
mazen506 <mazen506@gmail.com>
2021-12-14 16:03:31 +0300

    'pdf'

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
     'pdf' => array(
         'enabled' => true,
-        'binary' => 'vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf',
+        'binary' => base_path('vendor\\wemersonjanuario\\wkhtmltopdf-windows\\bin\\64bit\\wkhtmltopdf'),
         'timeout' => false,
         'images' => true,
         'options' => [
