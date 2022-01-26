d31675293194323af5b14094fe8246617b7200e0
mazen506 <mazen506@gmail.com>
2022-01-26 07:58:38 +0300

    'fix_images'

--- config/database.php
+++ config/database.php
@@ -55,20 +55,22 @@ return [
             // 'password' => env('DB_PASSWORD', 'admin1234'),
 
             //HEROKU
-            // 'url' => env('DATABASE_URL', 'mysql://b0442816b21331:3820b3ed@us-cdbr-east-04.cleardb.com/heroku_fe5cd69f57786c7?reconnect=true'),
-            // 'host' => env('DB_HOST', 'us-cdbr-east-04.cleardb.com'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'heroku_fe5cd69f57786c7'),
-            // 'username' => env('DB_USERNAME', 'b0442816b21331'),
-            // 'password' => env('DB_PASSWORD', '3820b3ed'),
+            'url' => env('DATABASE_URL', 'mysql://b0442816b21331:3820b3ed@us-cdbr-east-04.cleardb.com/heroku_fe5cd69f57786c7?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-east-04.cleardb.com'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'heroku_fe5cd69f57786c7'),
+            'username' => env('DB_USERNAME', 'b0442816b21331'),
+            'password' => env('DB_PASSWORD', '3820b3ed'),
 
             //LOCAL
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'mer_helper'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'admin90'),
+            // 'url' => env('DATABASE_URL'),
+            // 'host' => env('DB_HOST', 'localhost'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', 'mer_helper'),
+            // 'username' => env('DB_USERNAME', 'root'),
+            // 'password' => env('DB_PASSWORD', 'admin90'),
+
+
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
