3cf5118a55b0bd194140f0b7a34dba2e538c9906
unknown <mazen506@gmail.com>
2022-02-24 22:18:14 +0300

    'fixSecurity'

--- config/database.php
+++ config/database.php
@@ -48,28 +48,11 @@ return [
 
             //AWS
 
-            'host' => env('DB_HOST', 'shipping-mgr-db.c3i2dwc6kegp.us-east-2.rds.amazonaws.com'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_fe5cd69f57786c7'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'admin1234'),
-
-            //HEROKU
-            // 'url' => env('DATABASE_URL', 'mysql://b0442816b21331:3820b3ed@us-cdbr-east-04.cleardb.com/heroku_fe5cd69f57786c7?reconnect=true'),
-            // 'host' => env('DB_HOST', 'us-cdbr-east-04.cleardb.com'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'heroku_fe5cd69f57786c7'),
-            // 'username' => env('DB_USERNAME', 'b0442816b21331'),
-            // 'password' => env('DB_PASSWORD', '3820b3ed'),
-
-            //LOCAL
-            // 'url' => env('DATABASE_URL'),
-            // 'host' => env('DB_HOST', 'localhost'),
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'mer_helper'),
-            // 'username' => env('DB_USERNAME', 'root'),
-            // 'password' => env('DB_PASSWORD', 'admin90'),
-
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
 
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
