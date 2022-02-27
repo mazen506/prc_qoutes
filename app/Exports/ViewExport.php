ba1574947991bce2fec9bdb3fcea35bfeab22875
unknown <mazen506@gmail.com>
2022-02-27 09:05:01 +0300

    fix

--- app/Exports/ViewExport.php
+++ app/Exports/ViewExport.php
@@ -119,6 +119,7 @@ class ViewExport implements FromCollection, WithMapping, WithHeadings,WithCustom
                             $img = explode('|', $item->images)[0];
                             $source =  'storage/user_images/' . $img;
                             $stype = explode('.', $img)[1];
+                            dd($stype);
                             switch($stype) {
                                 case 'gif':
                                 $simg = imagecreatefromgif($source);
